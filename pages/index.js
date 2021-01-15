0de53a3feadb73f3929719781b88a9aba9a9f141
Chris McGrath <cmcgrath@oneintranets.com>
2021-01-14 21:42:47 -0800

    Fixed

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Look at me. I edited a page and deployed it.</a>
+          Look at me. I edited a page and deployed it.
         </h1>
 
         <p className={styles.description}>
